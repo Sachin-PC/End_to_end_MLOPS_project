0d9bde3ccfccdb6d058b1e78342cf5be3f5f704f
Sachin P C <sachinpc7@gmail.com>
2024-10-30 21:56:06 -0400

    implementred code with UI

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion stage"
+# STAGE_NAME = "Data Ingestion Stage"
+# try:
+#     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+#     obj = DataIngestionTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+# STAGE_NAME = "Data Validation Stage"
+# try:
+#     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+#     obj = DataValidationTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+# STAGE_NAME = "Data Transformation Stage"
+# try:
+#     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+#     obj = DataTransformationTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+# STAGE_NAME = "Model Trainer Stage"
+# try:
+#     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+#     obj = ModelTrainerTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+STAGE_NAME = "Model Evaluation Stage"
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
-    obj = DataIngestionTrainingPipeline()
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
 except Exception as e:
